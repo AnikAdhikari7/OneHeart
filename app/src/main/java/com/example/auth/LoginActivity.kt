ccadf77a46c7395f85b34af5e04b3cd32f882068
Anik Adhikari <developwithanik@gmail.com>
2023-05-18 14:04:33 +0530

    added logo

--- app/src/main/java/com/example/auth/LoginActivity.kt
+++ app/src/main/java/com/example/auth/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.oneheart.MainActivity
 import com.example.oneheart.R
 
 class LoginActivity : AppCompatActivity() {
@@ -15,6 +16,10 @@ class LoginActivity : AppCompatActivity() {
     fun createAnAccOnClick(view: View) {
         startActivity(Intent(this@LoginActivity,SignUpActivity::class.java))
         finish()
+    }
+
+    fun loginBtnOnClick(view: View) {
+        startActivity(Intent(this@LoginActivity,MainActivity::class.java))
     }
 
 }
